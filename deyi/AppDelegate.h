a3a8ed609de71be1cc510559e6245864ce5670ce
Liang <yangliangsky@gmail.com>
2018-08-01 10:49:14 +0800

    Initial Commit

---
+++ deyi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  deyi
+//
+//  Created by Liang on 2018/8/1.
+//  Copyright © 2018年 Liang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
